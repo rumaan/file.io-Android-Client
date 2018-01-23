d6943b96927da2ad4d46d49acd4c707203a0f110
Rumaan <rumaankalander@gmail.com>
2018-01-23 22:45:12 +0530

    Clean up and new builds

--- app/src/test/java/com/thecoolguy/rumaan/fileio/UrlTest.java
+++ app/src/test/java/com/thecoolguy/rumaan/fileio/UrlTest.java
@@ -9,6 +9,8 @@ import static junit.framework.Assert.assertEquals;
 
 public class UrlTest {
 
+    // TODO: check for more cases
+
     @Test
     public void check_UrlFromEncryptUrl() {
         String testUrl = "https://file.io/dfW34/dwnld";
