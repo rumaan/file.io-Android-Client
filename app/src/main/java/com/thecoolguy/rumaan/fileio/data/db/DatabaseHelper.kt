b806e2fbd79ec0c6509875939bf6abac4e4c07c1
Rumaan <rumaankalander@gmail.com>
2018-04-22 21:29:51 +0530

    Migrated all (most) background operations to RxJava2

--- app/src/main/java/com/thecoolguy/rumaan/fileio/data/db/DatabaseHelper.kt
+++ app/src/main/java/com/thecoolguy/rumaan/fileio/data/db/DatabaseHelper.kt
@@ -1,25 +1,37 @@
 package com.thecoolguy.rumaan.fileio.data.db
 
+import android.util.Log
 import com.thecoolguy.rumaan.fileio.data.models.FileEntity
-import kotlinx.coroutines.experimental.runBlocking
-import org.jetbrains.anko.coroutines.experimental.bg
+import com.thecoolguy.rumaan.fileio.repository.Repository
+import io.reactivex.Flowable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 
 object DatabaseHelper {
 
+    val TAG = DatabaseHelper.javaClass.simpleName
+
     fun saveToDatabase(fileEntity: FileEntity, mUploadItemDao: UploadItemDao) {
-        runBlocking {
-            val thread = bg {
-                mUploadItemDao.insert(fileEntity)
-            }
-        }
+        Flowable.fromCallable {
+            mUploadItemDao.insert(fileEntity)
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy {
+                    Log.d(TAG, "id: $it")
+                    Repository.getInstance().getAllItems()
+                }
+
+
     }
 
-    fun getAllItems(mUploadItemDao: UploadItemDao): MutableList<FileEntity>? {
-        return runBlocking {
-            bg {
-                mUploadItemDao.allUploads.value
-            }.await()
-        }
+
+    fun getAllItems(mUploadItemDao: UploadItemDao): Single<MutableList<FileEntity>> {
+        return mUploadItemDao.allUploads
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+
     }
 
 }
