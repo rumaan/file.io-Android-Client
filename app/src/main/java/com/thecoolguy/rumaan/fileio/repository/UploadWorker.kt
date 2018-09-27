e59dd87fdaf90f0c84014211a5a0e6055e7db12b
Rumaan <rumaankalander@gmail.com>
2018-09-27 19:54:47 +0530

    Fixed deprecated classes for workmanager.

--- app/src/main/java/com/thecoolguy/rumaan/fileio/repository/UploadWorker.kt
+++ app/src/main/java/com/thecoolguy/rumaan/fileio/repository/UploadWorker.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.toWorkData
+import androidx.work.workDataOf
 import com.thecoolguy.rumaan.fileio.data.db.DatabaseHelper
 import com.thecoolguy.rumaan.fileio.data.db.UploadHistoryRoomDatabase
 import com.thecoolguy.rumaan.fileio.data.models.FileEntity
@@ -16,45 +17,44 @@ import com.thecoolguy.rumaan.fileio.utils.Utils
 
 class UploadWorker : Worker() {
 
-  companion object {
-    const val KEY_URI = "file_uri"
-    const val KEY_RESULT = "file_url"
-    private val TAG = UploadWorker::class.simpleName
+    companion object {
+        const val KEY_URI = "file_uri"
+        const val KEY_RESULT = "file_url"
+        private val TAG = UploadWorker::class.simpleName
 
-  }
-
-  private fun save(fileEntity: FileEntity) {
-    val id = DatabaseHelper.saveToDatabase(
-        fileEntity, UploadHistoryRoomDatabase.getInstance(applicationContext).uploadItemDao()
-    )
-    Log.d(TAG, "Insert Id: $id")
-  }
-
-  override fun doWork(): WorkerResult {
-    val fileUri = inputData.getString(KEY_URI, null)
-    fileUri?.let { uri ->
-      // get the local file object from the backing storage
-      Utils.getLocalFile(applicationContext, Uri.parse(uri))
-          ?.let { localFile ->
-            // Upload the file
-            val (_, response, _) = Uploader.upload(localFile)
-
-            val fileEntity =
-              composeIntoFileEntity(Response.Deserializer().deserialize(response), localFile)
-
-            /* Save the uploaded file details into the LocalDb */
-            save(fileEntity)
-
-            // post a notification
-            NotificationHelper().create(applicationContext, fileEntity)
-
-            val output: Data = mapOf(KEY_RESULT to fileEntity.url).toWorkData()
-            outputData = output
-
-            return WorkerResult.SUCCESS
-          }
     }
-    return WorkerResult.FAILURE
-  }
+
+    private fun save(fileEntity: FileEntity) {
+        val id = DatabaseHelper.saveToDatabase(
+                fileEntity, UploadHistoryRoomDatabase.getInstance(applicationContext).uploadItemDao()
+        )
+        Log.d(TAG, "Insert Id: $id")
+    }
+
+    override fun doWork(): Result {
+        val fileUri = inputData.getString(KEY_URI)
+        fileUri?.let { uri ->
+            // get the local file object from the backing storage
+            Utils.getLocalFile(applicationContext, Uri.parse(uri))
+                    ?.let { localFile ->
+                        // Upload the file
+                        val (_, response, _) = Uploader.upload(localFile)
+
+                        val fileEntity =
+                                composeIntoFileEntity(Response.Deserializer().deserialize(response), localFile)
+
+                        /* Save the uploaded file details into the LocalDb */
+                        save(fileEntity)
+
+                        // post a notification
+                        NotificationHelper().create(applicationContext, fileEntity)
+                        val output: Data = workDataOf(KEY_RESULT to fileEntity.url)
+                        outputData = output
+
+                        return Result.SUCCESS
+                    }
+        }
+        return Result.FAILURE
+    }
 
 }
--- app/src/main/java/com/thecoolguy/rumaan/fileio/ui/activities/MainActivity.java
+++ app/src/main/java/com/thecoolguy/rumaan/fileio/ui/activities/MainActivity.java
@@ -22,7 +22,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.work.WorkStatus;
+
 import com.thecoolguy.rumaan.fileio.R;
 import com.thecoolguy.rumaan.fileio.data.models.LocalFile;
 import com.thecoolguy.rumaan.fileio.listeners.DialogClickListener;
@@ -36,7 +38,9 @@ import com.thecoolguy.rumaan.fileio.ui.fragments.UploadProgressFragment;
 import com.thecoolguy.rumaan.fileio.ui.fragments.UploadResultFragment;
 import com.thecoolguy.rumaan.fileio.utils.Utils.Android;
 import com.thecoolguy.rumaan.fileio.viewmodel.MainActivityViewModel;
+
 import org.jetbrains.annotations.NotNull;
+
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.OnNeverAskAgain;
 import permissions.dispatcher.OnPermissionDenied;
@@ -44,207 +48,207 @@ import permissions.dispatcher.RuntimePermissions;
 
 @RuntimePermissions
 public class MainActivity extends AppCompatActivity implements DialogClickListener,
-    OnFragmentInteractionListener, FileLoadListener {
+        OnFragmentInteractionListener, FileLoadListener {
 
-  // TODO: replace all Toasts with Snackbars
+    // TODO: replace all Toasts with Snackbars
 
-  public static final String TAG = "MainActivity";
-  private static final int INTENT_FILE_REQUEST = 44;
+    public static final String TAG = "MainActivity";
+    private static final int INTENT_FILE_REQUEST = 44;
 
-  private MainActivityViewModel viewModel;
-  private ConstraintLayout rootView;
-  private UploadProgressFragment progressFragment;
+    private MainActivityViewModel viewModel;
+    private ConstraintLayout rootView;
+    private UploadProgressFragment progressFragment;
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.options_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case R.id.menu_history:
-        startActivity(new Intent(this, UploadHistoryActivity.class));
-        return true;
-      case R.id.menu_about:
-        startActivity(new Intent(this, AboutActivity.class));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_main, menu);
         return true;
     }
-    return super.onOptionsItemSelected(item);
-  }
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode == INTENT_FILE_REQUEST && resultCode == RESULT_OK) {
-      Uri fileUri = data.getData();
-      if (fileUri != null) {
-        viewModel.chooseFileFromUri(this, fileUri);
-      } else {
-        Toast.makeText(this, getString(R.string.oops_some_error_occurred), Toast.LENGTH_SHORT)
-            .show();
-      }
-    }
-  }
-
-  @Override
-  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-      @NonNull int[] grantResults) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    MainActivityPermissionsDispatcher
-        .onRequestPermissionsResult(MainActivity.this, requestCode, grantResults);
-  }
-
-  @NeedsPermission({
-      Manifest.permission.READ_EXTERNAL_STORAGE,
-      Manifest.permission.WRITE_EXTERNAL_STORAGE
-  })
-  public void chooseFile() {
-    Intent intent = Android.INSTANCE.getChooseFileIntent();
-    startActivityForResult(Intent.createChooser(intent, "Choose the file to Upload.."),
-        INTENT_FILE_REQUEST);
-  }
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    setTheme(R.style.NoActionBarTheme);
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    rootView = findViewById(R.id.root_view);
-    Toolbar toolbar = findViewById(R.id.toolbar);
-    toolbar.setTitle("");
-    setSupportActionBar(toolbar);
-
-    // set up initial fragment
-    setUpInitialFragment();
-
-    viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
-  }
-
-  private void setUpInitialFragment() {
-    getSupportFragmentManager()
-        .beginTransaction()
-        .add(R.id.parent_fragment_container, ChooseFileFragment.newInstance(),
-            ChooseFileFragment.TAG)
-        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
-        .addToBackStack(ChooseFileFragment.TAG)
-        .commit();
-  }
-
-  @OnPermissionDenied({
-      Manifest.permission.READ_EXTERNAL_STORAGE,
-      Manifest.permission.WRITE_EXTERNAL_STORAGE
-  })
-  void showPermissionDeniedForStorage() {
-    Toast.makeText(this, getString(R.string.permission_deny), Toast.LENGTH_LONG).show();
-  }
-
-  @OnNeverAskAgain({
-      Manifest.permission.READ_EXTERNAL_STORAGE,
-      Manifest.permission.WRITE_EXTERNAL_STORAGE
-  })
-  void showAppSettings() {
-    Toast.makeText(this, getString(R.string.permission_deny), Toast.LENGTH_LONG).show();
-    Android.INSTANCE.showAppDetailsSettings(this);
-  }
-
-  @Override
-  public void onDialogPositiveClick(@NonNull Dialog dialog, @NonNull Fragment dialogFragment) {
-    if (dialogFragment instanceof NoNetworkDialogFragment) {
-      Android.INSTANCE.dismissDialog(dialog);
-    }
-  }
-
-  @Override
-  public void onChooseFileClick() {
-    MainActivityPermissionsDispatcher.chooseFileWithPermissionCheck(this);
-  }
-
-  @Override
-  public void onBackPressed() {
-    int backStackCount = getSupportFragmentManager().getBackStackEntryCount();
-    /* Pop until first fragment i.e (ChooseFileFragment) */
-    if (backStackCount > 1) {
-      getSupportFragmentManager()
-          .popBackStack(ChooseFileFragment.TAG, 0);
-    } else {
-      finish();
-    }
-  }
-
-  @Override
-  public void onUploadFileClick() {
-    viewModel.uploadFile();
-
-    // showSnackBar();
-    // replace the fragment container with progress
-
-    progressFragment = UploadProgressFragment.newInstance();
-    getSupportFragmentManager()
-        .beginTransaction()
-        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-        .replace(R.id.parent_fragment_container, progressFragment)
-        .addToBackStack(null)
-        .commit();
-
-    if (viewModel.getUploadWorkStatus() != null) {
-      viewModel.getUploadWorkStatus().observe(this, new Observer<WorkStatus>() {
-        @Override
-        public void onChanged(WorkStatus workStatus) {
-          if (workStatus.getState().isFinished()) {
-            String url = workStatus.getOutputData().getString(UploadWorker.KEY_RESULT);
-
-            // switch to results fragment
-            getSupportFragmentManager()
-                .beginTransaction()
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                .replace(R.id.parent_fragment_container, UploadResultFragment.newInstance(url),
-                    UploadResultFragment.TAG)
-                .addToBackStack(UploadResultFragment.TAG)
-                .commit();
-          }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_history:
+                startActivity(new Intent(this, UploadHistoryActivity.class));
+                return true;
+            case R.id.menu_about:
+                startActivity(new Intent(this, AboutActivity.class));
+                return true;
         }
-      });
+        return super.onOptionsItemSelected(item);
     }
-  }
 
-  @Override
-  public void onClose() {
-    /* Pop all fragments till (Exclusive of) ChooseFileFragment from back stack */
-    getSupportFragmentManager()
-        .popBackStack(ChooseFileFragment.TAG, 0);
-  }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == INTENT_FILE_REQUEST && resultCode == RESULT_OK) {
+            Uri fileUri = data.getData();
+            if (fileUri != null) {
+                viewModel.chooseFileFromUri(this, fileUri);
+            } else {
+                Toast.makeText(this, getString(R.string.oops_some_error_occurred), Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
 
-  private void showSnackBar() {
-    final Snackbar snackbar = Snackbar
-        .make(rootView, getString(R.string.file_uploaded_soon), Snackbar.LENGTH_INDEFINITE);
-    snackbar.setAction(getString(R.string.okay), new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        snackbar.dismiss();
-      }
-    });
-    snackbar
-        .setActionTextColor(ContextCompat.getColor(MainActivity.this, R.color.dark_yellow));
-    View snackBarView = snackbar.getView();
-    TextView snackTextView = snackBarView
-        .findViewById(android.support.design.R.id.snackbar_text);
-    snackTextView.setMaxLines(3);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher
+                .onRequestPermissionsResult(MainActivity.this, requestCode, grantResults);
+    }
 
-    snackbar.show();
-  }
+    @NeedsPermission({
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    })
+    public void chooseFile() {
+        Intent intent = Android.INSTANCE.getChooseFileIntent();
+        startActivityForResult(Intent.createChooser(intent, "Choose the file to Upload.."),
+                INTENT_FILE_REQUEST);
+    }
 
-  @Override
-  public void onFileLoad(@NotNull LocalFile localFile) {
-    // change the current fragment to upload
-    FragmentManager fragmentManager = getSupportFragmentManager();
-    FragmentTransaction transaction = fragmentManager.beginTransaction();
-    transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-    transaction.replace(R.id.parent_fragment_container,
-        UploadFileFragment.newInstance(localFile.getName()),
-        UploadFileFragment.TAG);
-    transaction.addToBackStack(UploadFileFragment.TAG);
-    transaction.commit();
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.NoActionBarTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rootView = findViewById(R.id.root_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+        // set up initial fragment
+        setUpInitialFragment();
+
+        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+    }
+
+    private void setUpInitialFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.parent_fragment_container, ChooseFileFragment.newInstance(),
+                        ChooseFileFragment.TAG)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+                .addToBackStack(ChooseFileFragment.TAG)
+                .commit();
+    }
+
+    @OnPermissionDenied({
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    })
+    void showPermissionDeniedForStorage() {
+        Toast.makeText(this, getString(R.string.permission_deny), Toast.LENGTH_LONG).show();
+    }
+
+    @OnNeverAskAgain({
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    })
+    void showAppSettings() {
+        Toast.makeText(this, getString(R.string.permission_deny), Toast.LENGTH_LONG).show();
+        Android.INSTANCE.showAppDetailsSettings(this);
+    }
+
+    @Override
+    public void onDialogPositiveClick(@NonNull Dialog dialog, @NonNull Fragment dialogFragment) {
+        if (dialogFragment instanceof NoNetworkDialogFragment) {
+            Android.INSTANCE.dismissDialog(dialog);
+        }
+    }
+
+    @Override
+    public void onChooseFileClick() {
+        MainActivityPermissionsDispatcher.chooseFileWithPermissionCheck(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        int backStackCount = getSupportFragmentManager().getBackStackEntryCount();
+        /* Pop until first fragment i.e (ChooseFileFragment) */
+        if (backStackCount > 1) {
+            getSupportFragmentManager()
+                    .popBackStack(ChooseFileFragment.TAG, 0);
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    public void onUploadFileClick() {
+        viewModel.uploadFile();
+
+        // showSnackBar();
+        // replace the fragment container with progress
+
+        progressFragment = UploadProgressFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .replace(R.id.parent_fragment_container, progressFragment)
+                .addToBackStack(null)
+                .commit();
+
+        if (viewModel.getUploadWorkStatus() != null) {
+            viewModel.getUploadWorkStatus().observe(this, new Observer<WorkStatus>() {
+                @Override
+                public void onChanged(WorkStatus workStatus) {
+                    if (workStatus.getState().isFinished()) {
+                        String url = workStatus.getOutputData().getString(UploadWorker.KEY_RESULT);
+
+                        // switch to results fragment
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .replace(R.id.parent_fragment_container, UploadResultFragment.newInstance(url),
+                                        UploadResultFragment.TAG)
+                                .addToBackStack(UploadResultFragment.TAG)
+                                .commit();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onClose() {
+        /* Pop all fragments till (Exclusive of) ChooseFileFragment from back stack */
+        getSupportFragmentManager()
+                .popBackStack(ChooseFileFragment.TAG, 0);
+    }
+
+    private void showSnackBar() {
+        final Snackbar snackbar = Snackbar
+                .make(rootView, getString(R.string.file_uploaded_soon), Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction(getString(R.string.okay), new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snackbar.dismiss();
+            }
+        });
+        snackbar
+                .setActionTextColor(ContextCompat.getColor(MainActivity.this, R.color.dark_yellow));
+        View snackBarView = snackbar.getView();
+        TextView snackTextView = snackBarView
+                .findViewById(android.support.design.R.id.snackbar_text);
+        snackTextView.setMaxLines(3);
+
+        snackbar.show();
+    }
+
+    @Override
+    public void onFileLoad(@NotNull LocalFile localFile) {
+        // change the current fragment to upload
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.replace(R.id.parent_fragment_container,
+                UploadFileFragment.newInstance(localFile.getName()),
+                UploadFileFragment.TAG);
+        transaction.addToBackStack(UploadFileFragment.TAG);
+        transaction.commit();
+    }
 }
