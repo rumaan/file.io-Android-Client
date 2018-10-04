8c50ba58ce2c9991f85be3eaca1d1dccbb76cf99
Deepak Kumar <dpk950@gmail.com>
2018-10-04 15:11:23 +0530

    Add LicenseActivity

--- app/src/main/java/com/thecoolguy/rumaan/fileio/ui/activities/AboutActivity.kt
+++ app/src/main/java/com/thecoolguy/rumaan/fileio/ui/activities/AboutActivity.kt
@@ -1,8 +1,10 @@
 package com.thecoolguy.rumaan.fileio.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
+import android.view.MenuItem
 import com.thecoolguy.rumaan.fileio.R
 import kotlinx.android.synthetic.main.activity_about.toolbar
 
@@ -20,4 +22,12 @@ class AboutActivity : AppCompatActivity() {
     setSupportActionBar(toolbar)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
   }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_open_source -> startActivity(Intent(this,LicenseActivity::class.java))
+            else -> return false
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
